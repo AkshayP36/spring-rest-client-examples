8400780d40ccbeb92520938226520f929b88d8ac
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2020-11-11 00:40:13 +0530

    Initial file commit

---
+++ src/main/java/guru/springframework/api/albums/Example.java
@@ -0,0 +1,46 @@
+
+package guru.springframework.api.albums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example {
+
+    private Integer userId;
+    private Integer id;
+    private String title;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
